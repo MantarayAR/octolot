ff887053310680815ad2bc5f53a1ed2273ab30ef
Ivan Montiel <idmontie@asu.edu>
2015-11-07 15:15:03 -0700

    Add logo to project

--- src/client/views/components/navigation-component.jsx
+++ src/client/views/components/navigation-component.jsx
@@ -3,11 +3,15 @@ NavigationComponent = React.createClass({
     return(
      <nav>
         <div className="nav-wrapper">
-          <a href="/" className="brand-logo">Octolot</a>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="/about">About</a></li>
-            <li><a href="/search">Search</a></li>
-          </ul>
+          <div className="row">
+            <div className="col s12">
+              <a href="/" className="brand-logo"><img className="nav__logo" src="/mstile-70x70.png" /> Octolot</a>
+              <ul id="nav-mobile" className="right hide-on-med-and-down">
+                <li><a href="/about">About</a></li>
+                <li><a href="/search">Search</a></li>
+              </ul>
+            </div>
+          </div>
         </div>
       </nav>
     );
