097d2ce578d9120fc6d029f081a775b420fb652b
Ivan Montiel <idmontie@asu.edu>
2015-11-07 17:44:38 -0700

    Add links to footer

--- src/client/views/components/footer-component.jsx
+++ src/client/views/components/footer-component.jsx
@@ -14,17 +14,15 @@ FooterComponent = React.createClass({
             <div className="col l4 offset-l2 s12">
                 <h5 className="white-text">Links</h5>
                 <ul>
-                  <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                  <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                  <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                  <li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                  <li><a className="grey-text text-lighten-3" href="https://github.com/MantarayAR/octolot" target="_tab">Github</a></li>
+                  <li><a className="grey-text text-lighten-3" href="/about">About</a></li>
                 </ul>
               </div>
           </div>
         </div>
         <div className="footer-copyright">
           <div className="container">
-            &copy; 2014 Copyright Mantaray AR LLC
+            &copy; 2015 Copyright Mantaray AR LLC
           </div>
         </div>
       </footer>
